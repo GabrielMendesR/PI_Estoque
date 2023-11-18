7e29dd70275d59f7f7e712a883d61164444b523f
GabrielMendesR <mendes@edu.unifil.br>
2023-11-17 21:14:50 -0300

    mendes

--- JS/database.js
+++ JS/database.js
@@ -42,7 +42,7 @@ db.get(
 );
 
 
-function addEstoque(){
+function addBanco(){
   console.log("teste")
   const sql1 = `INSERT INTO produtos(nome, categoria, modelo, fornecedor, preco) VALUES ('luis', 'categoria1', 'modelo1', 'fornecedor1', '30')` 
   db.run(sql1)
