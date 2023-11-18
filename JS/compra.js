93f28f62b4be6bf8e71982c9de55584c61876db3
Luis <luis.aquino@edu.unifil.br>
2023-11-18 20:28:00 -0300

    ajustado logica de compra e criada tela movimentacoes

--- JS/compra.js
+++ JS/compra.js
@@ -1,8 +1,33 @@
-counter = 0;
+document.addEventListener('DOMContentLoaded', function() {
+  getAllProductsRequest().then(data => {
+    console.log("data:",data)
+    addCodigo(data)
+  })
+});
 
-function addCodigo() {
-    document.getElementById('itens').innerHTML += "<option value='" + counter + "'>#" + counter + "</option>";
-    ++counter
+function addCodigo(data) {
+
+  const elemento = document.getElementById('itens') 
+
+  data.forEach(produto => {
+    const option = document.createElement('option');
+    option.value = produto.id; 
+    option.text = `${produto.id} - ${produto.nome}`; 
+    elemento.appendChild(option); 
+  });
 };
 
-addCodigo()
+async function getAllProductsRequest() {
+  const response = await fetch('http://localhost:3000/api/get-all-products', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok.');
+  }
+  const data = await response.json(); // Parse the JSON data from the response
+  return data.data; 
+}
