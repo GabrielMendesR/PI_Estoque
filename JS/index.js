1fe838f040771860413206f803e0e0745f1e2592
luis <luis.unifil>
2023-11-17 21:14:15 -0300

    movido database para index

--- JS/index.js
+++ JS/index.js
@@ -5,6 +5,51 @@
 
 // });
 
+const sqlite3 = require("sqlite3").verbose();
+
+let sql
+  
+const db = new sqlite3.Database("../database/estoque.db", sqlite3.OPEN_READWRITE, (err) => {
+  if(err) console.log(err.message)
+})
+
+
+db.get(
+  "SELECT name FROM sqlite_master WHERE type='table' AND name='produtos'",
+  (err, row) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      if (row) {
+        console.log('Table "produtos" already exists');
+        // Table exists, perform actions accordingly
+      } else {
+        // Table does not exist, create it
+        const sql = `CREATE TABLE produtos (
+          id_produto INTEGER PRIMARY KEY,
+          nome TEXT,
+          categoria TEXT,
+          modelo TEXT,
+          fornecedor TEXT,
+          preco REAL,
+          quantidade INTEGER,
+          valor_total INTEGER
+        )`;
+
+        db.run(sql, (createErr) => {
+          if (createErr) {
+            console.error(createErr.message);
+          } else {
+            console.log('Table "produtos" created');
+          }
+        });
+      }
+    }
+  }
+);
+
+
+
 class Prod{
     constructor(nome, categ, forn, mod, prec) {
         this.nome = nome;
