b9da6e2ac523b4f191cfcfd78f6c1ba388af42e1
GabrielMendesR <mendes@edu.unifil.br>
2023-11-17 21:10:03 -0300

    Mendes

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,27 @@
 
 class Prod{
-    constructor(nome, categ, forn, mod) {
+    constructor(nome, categ, forn, mod, prec) {
         this.nome = nome;
         this.categoria = categ;
         this.fornecedor = forn;
         this.quantidade = 0;
         this.modelo = mod;
+        this.preco = prec;
 
     };
 };
 
-produto = new Prod('raquete', 'esporte', 'atacadao', 'v1');
+function addEstoque() {
+    var prod = [document.getElementById('nome').value, document.getElementById('categoria').value, document.getElementById('fornecedor').value, document.getElementById('modelo').value, document.getElementById('preco').value];
+    if (prod[0] != '' && prod[1] != '' && prod[2] != '' && prod[3] != '' && (prod[4] != '')) {
+        const produto = new Prod(prod[0], prod[1], prod[2], prod[3], parseFloat(prod[4]))            
+            
+        console.log(produto);
+    }
+}
 
-console.log(produto.nome);
-console.log(produto.categoria);
-console.log(produto.quantidade);
+document.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        addEstoque();
+    }
+})
